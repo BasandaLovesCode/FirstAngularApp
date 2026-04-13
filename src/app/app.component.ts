4b2199b499567fd3e554c0634db6a5765c817d9c
Basanda Shabalala <bsdshabalala11@gmail.com>
2026-04-14 00:34:32 +0200

    Added logic to the Login Page. Also included ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'myFirstApp';
+  //variables inside a class a class variables therefore we
+//dont use let keyword to declare
+username:string = '';
+password:string='';
+errmessage:string='';
+//hardcode the values for now
+//if we dont want these to be modified from outside
+//component we can declare them as private and readonly
+realusername:string='inf354';
+realpassword:string='abcde';
+//logic of program , in login function/method
+login()
+{
+if(this.username ==this.realusername &&
+this.password==this.realpassword)
+this.errmessage = "Login successful";
+else
+this.errmessage="Login unsuccesful";
+}
 }
